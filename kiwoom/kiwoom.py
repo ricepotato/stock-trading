af470dcc523a9fdc2e560dc84ab3b1bfde44890f
ricepotato <sukjun40@naver.com>
2020-06-21 22:52:28 +0900

    kiwoom login

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,5 +1,6 @@
 import logging
-from PyQt5.QAxContainer import QAxWidget
+from PyQt5.QAxContainer import QAxWidget, QEventLoop
+from config.errorCode import errors
 
 log = logging.getLogger("app")
 
@@ -10,6 +11,20 @@ class Kiwoom(QAxWidget):
         log.info("Kiwoom class Start")
 
         self.get_ocx_instance()
+        self.event_slots()
+        self.signal_login_commConnect()
+        self.login_event_loop = QEventLoop()
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+
+    def login_slot(self, err_code):
+        log.info(errors(err_code[1]))
+        self.login_event_loop.exit()
+
+    def signal_login_commConnect(self):
+        self.dynamicCall("CommConnect()")
+        self.login_event_loop.exec_()
